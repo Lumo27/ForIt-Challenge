008b56e923955c7683fbbd7fb2e30e81e30c711e
Lucas <116810958+Lumo27@users.noreply.github.com>
2025-02-24 14:56:52 -0300

    Comentarios sobre flujo del algoritmo.

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
-
+// Se conecta con fetch a la info, la guarda, o muestra error.
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((data) => setUsers(data))
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
-  console.log(users);
-
+//Return para procesar y renderizar usuarios
   return (
-    <div className="container">
+    //Container para meter cards
+<div className="container">
+      {/* Aca se hace un map, para recorrer todos los usuarios conseguidos, y se usa desestructuraciones para facilitar el ingreso a la info. */}
       {users.map(({ id, name, username, email, address: { city }, phone, company: { name: companyName } }) => (
   <div key={id} className="card">
     <div className="card-header">
@@ -27,9 +28,9 @@ function App() {
       <p><strong>Empresa:</strong> {companyName}</p>
     </div>
   </div>
-))}
-    </div>
-  );
+  ))}
+</div>
+);
 }
 
 export default App;
